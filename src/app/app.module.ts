2b91139c8017b152dc0491da606d039e4530284e
Nico <nicoassanelli@hotmail.com>
2020-04-12 19:30:02 -0300

    Agregado componente alta alumno
    Se agrego el componente alta alumno

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AltaAlumnoComponent } from './alta-alumno/alta-alumno.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AltaAlumnoComponent
   ],
   imports: [
     BrowserModule,
