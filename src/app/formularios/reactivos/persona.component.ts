47ea128e553c6d8df8bed95758a3ff93fe7a048a
Nico <nicoassanelli@hotmail.com>
2020-04-18 17:18:54 -0300

    Se agregaron ejemplos con formGroup

--- src/app/formularios/reactivos/persona.component.ts
+++ src/app/formularios/reactivos/persona.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-reactive-persona',
     templateUrl: './persona.component.html'
 })
 export class PersonaComponent {
-    nombreControl = new FormControl('');
-    apellidoControl = new FormControl('');
-    edadControl = new FormControl('11');
+   // nombreControl = new FormControl('');
+   // apellidoControl = new FormControl('');
+   // edadControl = new FormControl('55');
+
+   personaForm = new FormGroup({
+       nombre: new FormControl (''),       
+       apellido: new FormControl (''),
+       direccion: new FormControl(''),
+       localidad: new FormControl(''),
+       edad: new FormControl(''),
+   });
+
+   guardarPersona() {
+       debugger;
+       this.personaForm.value;
+       console.warn(this.personaForm.value)
+   }
+
+
 }
